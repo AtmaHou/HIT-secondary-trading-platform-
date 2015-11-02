587e7646e08eb0aab41307e22e909da3e613eec4
lexuslee <lilingzhi_hit@163.com>
2015-11-02 22:15:50 +0800

    fix some bug about models,except uploding picture

--- HIT2nd/HSTP/models.py
+++ HIT2nd/HSTP/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-import datetime
+from datetime import datetime
 # Create your models here.
 class Client(models.Model):
-    email = models.EmailField(primary_key=True,unique=True) #唯一标识
+    email = models.EmailField(primary_key=True,unique=True) #only tag
     password = models.IntegerField()    #!!!
     realname = models.CharField(max_length=30)
     nickname = models.CharField(max_length=30)
-    register_date = models.DateTimeField(default=datetime.now)  #自动生成    
-    IDcard = models.IntegerField(max_length=18)       #身份证号
+    register_date = models.DateTimeField(default=datetime.now())  #auto    
+    IDcard = models.IntegerField(max_length=18)       #IDcard
     studentID = models.IntegerField(max_length=10) #!!!
     colledge = models.CharField(max_length=50)
     school = models.CharField(max_length=50)
@@ -16,33 +16,33 @@ class Client(models.Model):
     telephone = models.IntegerField(max_length=15)   #!!!
     sex = models.BooleanField(default=True)
     is_identified = models.BooleanField(default=False)
-    seller_level = models.IntegerField(default=1)    #自动生成1 
-    buyer_level = models.IntegerField(default=1)    #自动生成1 
+    seller_level = models.IntegerField(default=1)    #auto
+    buyer_level = models.IntegerField(default=1)    #auto
     is_lonly_dog = models.BooleanField(default=True)
 
 class Product(models.Model):
-    productID = models.DateTimeField(primary_key=True,unique=True,default=datetime.now)   #唯一标识  上架日期 20151101220616
+    productID = models.DateTimeField(primary_key=True,unique=True,default=datetime.now())   #only tag 20151101220616
     name = models.CharField(max_length=80)     
     price = models.FloatField()   #!!!
-    client = models.ForeignKey(Client,related_name="products")        #识别卖家
+    client = models.ForeignKey(Client,related_name="products")        
     trading_place = models.CharField(max_length=80)  
     is_identified = models.BooleanField(default=False)
     introduction = models.CharField(max_length=200) 
     is_reserved = models.BooleanField(default=False)
-    collected_clients = models.ManyToMany(Client,related_name="collect_products")
+    collected_clients = models.ManyToManyField(Client,related_name="collect_products")
     
 class Comment(models.Model):
-    product = models.ForeignKey(Product,primary_key=True,related_name="comments")  #商品
-    client = models.ForeignKey(Client,related_name="comments")    #评论者
+    product = models.ForeignKey(Product,primary_key=True,related_name="comments")  
+    client = models.ForeignKey(Client,related_name="comments")    #comment man
     content = models.CharField(max_length=200) 
-    comment_date = models.DateTimeField(default=datetime.now) 
+    comment_date = models.DateTimeField(default=datetime.now()) 
     
 class Category(models.Model):
-    product = models.ForeignKey(Product,primary_key=True,related_name="categories")    #识别商品
-    category = models.CharField(max_length=40)     #商品类别
+    product = models.ForeignKey(Product,primary_key=True,related_name="categories")   
+    category = models.CharField(max_length=40)     #type
     
 
 class Label(models.Model):
-    product = models.ForeignKey(Product,max_length=14,primary_key=True,related_name="labels")    #识别商品
-    label = models.CharField(max_length=40)    #商品标签
+    product = models.ForeignKey(Product,max_length=14,primary_key=True,related_name="labels")    
+    label = models.CharField(max_length=40)    #tag
     
